dc40f553335f78101ea2f1f5cd1c718119aa90fb
khodesaeed <khodesaeedkazemi@gmail.com>
2021-06-09 16:40:36 +0430

    add router to app

--- app.js
+++ app.js
@@ -2,12 +2,18 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const { sequelize } = require('./models');
-const router = require('./router/router')
+const authRouter = require('./router/authRouter');
+const folderRouter = require('./router/folderRouter');
+const taskRouter = require('./router/taskRouter');
+const userRouter = require('./router/userRouter');
 
 const port = process.env.PORT;
 
 app.use(express.json());
-app.use('/', router);
+app.use('/api/user', userRouter);
+app.use ('/api/auth', authRouter);
+app.use('/api/folder', folderRouter);
+app.use('/api/task', taskRouter);
 
 app.listen(port, async() => {
     console.log(`Server running on port ${port}`);
