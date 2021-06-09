42a38ae172bd33e291d9d336fa516f68c861ea45
khodesaeed <khodesaeedkazemi@gmail.com>
2021-06-09 16:41:13 +0430

    change auth folder

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,29 +1,35 @@
-require('dotenv').config();
-const { User } = require('../models');
+const {
+    User
+} = require('../models');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const tokenSecret = process.env.TOKEN_SECRET;
 
 function genToken(userData) {
-    const { username, role_name } = userData;
-    return jwt.sign({ username, role_name }, tokenSecret, { expiresIn: '15m' });
+    const {
+        username,
+        role_name
+    } = userData;
+    return jwt.sign({
+        username,
+        role_name
+    }, tokenSecret, {
+        expiresIn: '15m'
+    });
 };
 
-function hasToken(req, res, next) {
-    const token = req.headers.authorization.split(' ')[1];
-    if (!token) throw new Error('Please login...');
-    next();
-
-};
-
-function authorize() {
-
-}
 
 async function authenticate(user) {
     try {
-        const { username, password } = user;
-        const findUser = await User.findOne({ where: { username } });
+        const {
+            username,
+            password
+        } = user;
+        const findUser = await User.findOne({
+            where: {
+                username
+            }
+        });
         if (findUser == null) throw new Error('user not found');
         if (!await bcrypt.compare(password, findUser.password)) throw new Error(('Invalid password'));
         return {
@@ -37,18 +43,28 @@ async function authenticate(user) {
 
 async function signup(req, res) {
     try {
-        const { username, password, role_name } = req.body;
+        const {
+            username,
+            password,
+            role_name
+        } = req.body;
         const saltRounds = await bcrypt.genSalt();
         const hash = await bcrypt.hash(password, saltRounds);
         try {
-            const user = await User.create({ username, password: hash, role_name });
+            const user = await User.create({
+                username,
+                password: hash,
+                role_name
+            });
             res.send(user)
         } catch (err) {
             console.log(err);
             return res.status(500).json(err);
         }
     } catch (hasherr) {
-        res.status(500).json({ error_message: 'somthing went wrong in signup process!' });
+        res.status(500).json({
+            error_message: 'somthing went wrong in signup process!'
+        });
     }
 };
 
@@ -57,7 +73,9 @@ async function login(req, res) {
         const userData = await authenticate(req.body);
         //TODO create role model and check the token in each API endpoint
         const token = genToken(userData);
-        res.status(200).json({ token })
+        res.status(200).json({
+            token
+        })
     } catch (error) {
         res.status(500).json(error);
     };
@@ -66,7 +84,11 @@ async function login(req, res) {
 async function makeAdmin(req, res) {
     try {
         const userUuid = req.params.uuid
-        const user = await User.update({ where: { uuid: userUuid } });
+        const user = await User.update({
+            where: {
+                uuid: userUuid
+            }
+        });
         res.send(user);
     } catch (err) {
         res.status(500).json(err);
